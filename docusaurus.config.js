d4f5ad521afe08b71870307db22575bd67d9e8cb
Revital <revital@allegro.ai>
2022-05-16 08:06:11 +0300

    release notes & AMIs 1.5

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -104,7 +104,7 @@ module.exports = {
             },
             {
               label: 'Release Notes',
-              to: '/docs/release_notes/ver_1_4',
+              to: '/docs/release_notes/ver_1_5',
             },
             {
               label: 'Community Resources',
