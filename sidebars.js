ddb2d2e9a07b47aeb62536b9b227bcb8fcfb8995
pollfly <75068813+pollfly@users.noreply.github.com>
2023-08-10 11:49:05 +0300

    Rewrite Keras integration page (#641)

--- sidebars.js
+++ sidebars.js
@@ -62,7 +62,7 @@ module.exports = {
                 'guides/frameworks/autokeras/integration_autokeras',
                 'integrations/catboost', 'integrations/click', 'guides/frameworks/fastai/fastai_with_tensorboard',
                 'integrations/hydra',
-                'guides/frameworks/keras/keras_tensorboard', 'guides/frameworks/tensorflow/integration_keras_tuner',
+                'integrations/keras', 'guides/frameworks/tensorflow/integration_keras_tuner',
                 'integrations/lightgbm', 'integrations/matplotlib',
                 'integrations/megengine', 'integrations/openmmv', 'integrations/optuna',
                 'integrations/python_fire', 'integrations/pytorch',
