6b02f02b0da26289f349f53e7ab1214a2f27f551
pollfly <75068813+pollfly@users.noreply.github.com>
2023-10-01 09:43:37 +0300

    Add ClearML 1.13.0+1.13.1 release notes (#675)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -93,7 +93,7 @@ module.exports = {
 
             {
               label: 'Release Notes',
-              to: '/docs/release_notes/ver_1_12',
+              to: '/docs/release_notes/ver_1_13',
             },
             {
               label: 'Community Resources',
